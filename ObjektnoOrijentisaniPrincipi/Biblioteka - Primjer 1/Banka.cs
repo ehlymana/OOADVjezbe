acaf6b1f739d835975d1ea69acbd92243cf04b1f
Ehlimana <ekrupalija1@etf.unsa.ba>
2021-02-09 19:32:54 +0100

    Sistem klasa za rad sa bankom

---
+++ ObjektnoOrijentisaniPrincipi/Biblioteka - Primjer 1/Banka.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteka
+{
+    public class Banka
+    {
+        #region Atributi
+
+        List<Klijent> klijenti = new List<Klijent>();
+        List<Racun> racuni = new List<Racun>();
+
+        #endregion
+
+        #region Metode
+
+        public void DodajKlijenta(Klijent osoba)
+        {
+            klijenti.Add(osoba);
+        }
+
+        public void OtvoriRacunZaOsobu(Klijent osoba)
+        {
+            Racun racun = new Racun(0)
+            {
+                Id = racuni.Count
+            };
+            racuni.Add(racun);
+            osoba.Racuni.Add(racun);
+        }
+
+        public Klijent PronadiKlijenta(int identitet)
+        {
+            return klijenti.Find(osoba => osoba.Identitet == identitet);
+        }
+
+        public void PrebaciNovac(Racun racun, decimal kolicina)
+        {
+            racun.PoloziNovac(kolicina);
+        }
+
+        #endregion
+    }
+
+
+}
---
+++ ObjektnoOrijentisaniPrincipi/Biblioteka - Primjer 3/IPodaci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteka
+{
+    public interface IPodaci
+    {
+        public int GenerišiID()
+        {
+            int broj = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Random generator = new Random();
+                broj += (int) Math.Pow(10, i) * generator.Next(0, 9);
+            }
+            return broj;
+        }
+
+        string DajOpis();
+    }
+}
