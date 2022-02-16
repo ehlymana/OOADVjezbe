dfb292a7a79aa1bdb65f8738f31c9bf7c1e4ad6c
Ehlimana <ekrupalija1@etf.unsa.ba>
2022-02-16 15:31:46 +0100

    .NET 5

---
+++ Rutiranje/PrvaASPNETAplikacija/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrvaASPNETAplikacija.Models
+{
+    public class Student
+    {
+        [Key]
+        public int BrojIndeksa { get; set; }
+        public VrstaStudenta Vrsta { get; set; }
+        public int GodinaStudija { get; set; }
+
+        public Student() { }
+    }
+}
