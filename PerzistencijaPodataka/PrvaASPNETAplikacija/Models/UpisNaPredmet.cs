27705a8d5e2e38d80c7066273d1270c72d43caa8
Ehlimana <ekrupalija1@etf.unsa.ba>
2022-02-16 14:02:00 +0100

    .NET 5

---
+++ PerzistencijaPodataka/PrvaASPNETAplikacija/Models/UpisNaPredmet.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrvaASPNETAplikacija.Models
+{
+    public class UpisNaPredmet
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+
+        [ForeignKey("Predmet")]
+        public int PredmetId { get; set; }
+
+        public Student Student { get; set; }
+
+        public Predmet Predmet { get; set; }
+
+        public UpisNaPredmet() { }
+    }
+}
---
+++ PerzistencijaPodataka/PrvaASPNETAplikacija/Models/VrstaStudenta.cs
@@ -0,0 +1,7 @@
+﻿namespace PrvaASPNETAplikacija.Models
+{
+    public enum VrstaStudenta
+    {
+        Redovni, RedovniSF, Vanredni
+    }
+}
