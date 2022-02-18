7de6af535fd71751e8c8239f6de10b9132092bb6
Ehlimana <ekrupalija1@etf.unsa.ba>
2022-02-18 21:29:48 +0100

    .NET 5

---
+++ VanjskiUredaji/PrvaASPNETAplikacija/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrvaASPNETAplikacija.Models
+{
+    public class Student
+    {
+        [Key]
+        public int BrojIndeksa { get; set; }
+
+        [EnumDataType(typeof(VrstaStudenta))]
+        public VrstaStudenta Vrsta { get; set; }
+
+        public int GodinaStudija { get; set; }
+
+        public string Slika { get; set; }
+
+        public Student() { }
+    }
+}
