3e22fd8f776290e91be74543a25d01084bc12383
Ehlimana <ekrupalija1@etf.unsa.ba>
2022-02-24 18:15:35 +0100

    .NET 5 - singleton

---
+++ KreacijskiPaterni/Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Paint paint = new Paint();
+            Oblik crveniTrougao = new Oblik("trougao", "crvena", "crna", "linija", 10, 1, 0, 0, 100, 150);
+
+            paint.kreirajNoviOblik(crveniTrougao);
+            paint.kopirajOblik(crveniTrougao);
+
+            Console.Out.WriteLine("Oblici: ");
+            foreach (Oblik oblik in paint.Oblici)
+                Console.Out.WriteLine("X: " + oblik.X + ", Y: " + oblik.Y);
+        }
+    }
+}
---
+++ KreacijskiPaterni/Singleton/Vlada.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Vlada
+    {
+        PremijerSingleton premijer = new PremijerSingleton();
+
+        public bool izaberiPremijera(string ime, string prezime, List<bool> saglasnosti)
+        {
+            if (saglasnosti.FindAll(x => x == true).Count > 0.67 * saglasnosti.Count)
+            {
+                premijer.izaberiPremijera(ime, prezime);
+                return true;
+            }
+            return false;
+        }
+    }
+}
